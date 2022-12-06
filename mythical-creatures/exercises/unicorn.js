529f0886899153ceebe2ca3b6bb2be9d118404af
Shane Misra <shane.misra@gmail.com>
2022-12-05 20:19:20 -0700

    Find Unicorn complete

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,45 @@
 class Unicorn {
   constructor(name, color) {
+    console.log(color)
+    if (color === undefined) {
+      this.color = 'white';
+    }
+    else {
+      this.color = color;
+    }
     this.name = name;
-    this.color = color;
+  }
+  isWhite() {
+    return false
+  }
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
-
 module.exports = Unicorn;
+
+
+// 1. Read the test
+// 2. ask yourself if it's going to fail, and why
+// 3. run the rest
+// 4. make it pass
+// 
+// 
+//
+//
+//
+//
+//
+//
+// class, using a constructor:
+// class Dog {
+//   constructor(name) {
+//       // any traits or properties
+//       this.name = name;
+//   }
+
+    // behavior
+//   bark() {
+//     console.log('woof');
+//   }
+// }

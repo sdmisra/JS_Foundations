8e107a9e8cbf384af91acc60064b4fd4ae3b1a9d
Shane Misra <shane.misra@gmail.com>
2022-12-05 22:18:53 -0700

    Yarrrr time for bed

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -2,12 +2,34 @@ class Pirate {
     constructor(name, job) {
         this.name = name;
         if (job === undefined) {
-          this.job = 'scallawag';
+            this.job = 'scallywag';
         }
         else {
-          this.job = job;
+            this.job = job;
         }
-
+        this.cursed = false;
+        this.booty = 0;
+    }
+    robShip() {
+        if (this.booty >= 500) {
+            this.cursed = true;
+            return 'ARG! I\'ve been cursed!'
+        }
+        else {
+            this.booty += 100;
+            return 'YAARRR!';
+        }
+    
+    }
+    liftCurse() {
+        if (this.cursed === false) {
+        return 'You don\'t need to lift a curse!'
+        }
+        else {
+            this.booty -= 300;
+            this.cursed = false;
+        return 'Your curse has been lifted!'
+        }
     }
 
 

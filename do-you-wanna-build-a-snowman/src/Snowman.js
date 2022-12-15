4ce08c94ef521b49d6ffb132be5904bd80072c64
Shane Misra <shane.misra@gmail.com>
2022-12-15 08:34:40 -0700

    Snowman update

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,4 +1,18 @@
 class Snowman {
+    constructor(snowmanDetails) {
+        this.carrots = snowmanDetails.carrots;
+        this.coal = snowmanDetails.coal;
+        this.buttons = snowmanDetails.buttons;
+        this.snowballs = snowmanDetails.snowballs;
+        this.magicHat = false;
+    }
+    canWearMagicHat() {
+        if (this.coal >= 2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >= 2) {
+        this.magicHat = true;
+        return this.magicHat;
+        }
+        else {return this.magicHat}
+    }
 
 }
 

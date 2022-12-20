19905a7c3a0a0357e6469e9f7f829fe4cead6215
Shane Misra <shane.misra@gmail.com>
2022-12-20 16:53:30 -0700

    Protect the starks - direwolf complete

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,17 +1,33 @@
 
 class Direwolf {
-    constructor() {
-        
+    constructor(wolfName, newHome, otherSize) {
+        this.name = wolfName;
+        this.home = 'Beyond the Wall';
+        if (newHome) {
+          this.home = newHome;
+        }
+        this.size = 'Massive';
+        if (otherSize) {
+          this.size = otherSize;
+        }
+        this.starksToProtect = [];
+        this.huntsWhiteWalkers = true;
     }
-
-
-
-
-
-
-
-
-
+    protect(targetStark) {
+        if (this.starksToProtect.length <2 && this.home === targetStark.location) {
+            targetStark.safe = true;
+            this.starksToProtect.push(targetStark);
+            this.huntsWhiteWalkers = false;
+        }
+    }
+    leave(targetStark) {
+        for (var i=0; i < this.starksToProtect.length; i++) {
+            if (targetStark.name === this.starksToProtect[i].name) {
+                this.starksToProtect.splice(i, 1);
+                targetStark.safe = false;
+            }
+        }
+    }
 }
 
 module.exports = Direwolf;
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,9 +1,27 @@
-
+const Direwolf = require('../exercises/direwolf');
 class Stark {
-
-
-
-    
+    constructor(thisStark) {
+        this.name = thisStark.name;
+        if (thisStark.area) {
+            this.location = thisStark.area;
+        }
+        else {
+            this.location = 'Winterfell'
+        }
+        this.safe = false;
+    }
+    sayHouseWords() {
+    if (this.safe === false)
+        return `Winter is Coming`
+    else 
+        return 'The North Remembers'
+    }
+    callDirewolf(name, callHome) {
+        var suddenWolf = new Direwolf(name, callHome)
+        suddenWolf.home = this.location;
+        suddenWolf.protect(this)
+        return suddenWolf;
+    }
 }
 
 module.exports = Stark;
